6cfdca10b29f6046ee2cf712b001d0262aa5db15
SLEIPNOR <68347839+SLEIPNOR@users.noreply.github.com>
2021-12-17 17:16:37 +0000

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -2,109 +2,160 @@
 """
 Created on Thu Nov 25 02:27:34 2021
 
-@author: Blade
+@author: Z.Han
 """
+""" Hello, I'm so happy you can use my code. I have prepared all preprocessed data so you 
+can use this code to reproduce the grid search and training process directly. 
+Just run each code block sequently by press "ctrl + enter".
+ If you only want to test the final model with the best C and gamma, just run 3rd code 
+ block "formal training and testing" """
+#%% import module & def func
 
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import svm, datasets
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-from sklearn.metrics import classification_report,accuracy_score
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import KFold
-import pandas as pd
-#%% data preprocessing
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
 
-X_train = np.array(pd.read_csv('./x_train_pca.csv',header = None))
-y_train = np.array(pd.read_csv('./y_train.csv',header = None)).ravel()
-X_test = np.array(pd.read_csv('./x_test_pca.csv',header = None))
-y_test = np.array(pd.read_csv('./y_test.csv',header = None)).ravel()
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# define SVM func
+def SVM(X_train,y_train, X_test, gamma,C):
+    model = svm.SVC(C=C, gamma = gamma,kernel='rbf')
+    # model = svm.SVC(C=1, kernel='linear') # test linear
 
-# transform label into 1/0
-i = 0
-for label in y_train:
-    if label == 1:
-        y_train[i] = 1
-        
-    elif label == 2:
-        y_train[i] = 1
-        
-    elif label == 3:
-        y_train[i] = 1
-        
-    i = i+1
-
-i = 0    
-for label in y_test:
-    if label == 1:
-        y_test[i] = 1
-        
-    elif label == 2:
-        y_test[i] = 1
-        
-    elif label == 3:
-        y_test[i] = 1
-        
-    i = i+1
-    
-    
-#%% Use SVM from a library
-
-def SVM(X_train,y_train, X_test, gamma):
-    model = svm.SVC(C=1, gamma = gamma)
     model.fit(X_train,y_train)
-    y_pred = model.predict(X_test)
-    return y_pred
-#%%
-gamma_list = np.linspace(0.1,1,20)
-n_splits = 10
-kf=KFold(n_splits=n_splits,random_state=0,shuffle=True)
-gamma_candidate = []
-i = 0
-for gamma in gamma_list:
-    
-    score = 0
-    for train_index,valid_index in kf.split(X_train):
-        y_pred = SVM(X_train[train_index], y_train[train_index], X_train[valid_index],gamma)
-        score =score + accuracy_score(y_train[valid_index],y_pred)
-        i = i+1
-        print('\r'"Cross Validation Process:{0}%".format(round(i *100 / (len(gamma_list)*n_splits))), end="",flush=True)
-       
-      
-    avg_score = score/kf.n_splits
-    gamma_candidate.append(avg_score)
-    
-gamma_best = gamma_list[gamma_candidate.index(max(gamma_candidate))]
-print('\nBest Gamma: '+str(gamma_best))
 
-#%%
-plt.plot(gamma_list,gamma_candidate)
+    y_pred = model.predict(X_test)  # test data prediction
+    y_pred_T = model.predict(X_train) # training data prediction
 
-# Scikit learn library results
-y_pred=SVM(X_train,y_train, X_test,gamma_best)
-print(accuracy_score(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+    return y_pred, y_pred_T
 
 
+# %% five-fold cross-validation
+""" code below implement a gird search for C and gamma in five-fold 
+cross-validation and also plot a 3D figure"""
+
+# grid search range
+C_list= [1e-2,1e-1,1e0,1e1,2e1,3e1,4e1,5e1] #  Regularization
+gamma_list = [1e-3,1e-2,1e-1,1e0,1e1,2e1,3e1,4e1,5e1] # gamma for kernel func
+
+# grid search start
+score_avg = list()
+for C in C_list:
+    print("C candidate {}".format(C))
+    for gamma in gamma_list:
+
+        print("gamma candidate {}".format(gamma))
+        score = list()
+
+        # 5-fold cross-validation
+        for i in range(5):
+            print("fold {}".format(i))
+
+            # load preprocessing data in i-fold
+            X_train = np.array(pd.read_csv('./x_train_pca_{}.csv'.format(i),
+                                           header=None))
+            y_train = np.array(pd.read_csv('./y_train_{}.csv'.format(i),
+                                           header=None)).ravel()
+            X_test = np.array(pd.read_csv('./x_test_pca_{}.csv'.format(i),
+                                          header=None))
+            y_test = np.array(pd.read_csv('./y_test_{}.csv'.format(i),
+                                          header=None)).ravel()
+
+            # normalize
+            scaler = MinMaxScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.transform(X_test)
+
+            # training & prediction
+            y_pred, _ = SVM(X_train,y_train, X_test,gamma,C)
+
+            score.append(accuracy_score(y_test, y_pred)) # collect acc score
+
+            print('Accuracy: ' + str(accuracy_score(y_test, y_pred)))
+        score_avg.append(np.mean(score))  # collect avg acc score
+        print('avg Accuracy: ' + str(score_avg[gamma_list.index(gamma)]))
+
+    # print('avg Accuracy: ' + str(score_avg[C_list.index(C)]))
+# np.savetxt('grid_search.csv', score_avg, delimiter = ',')
+# score_avg = np.array(pd.read_csv('grid_search.csv',header=None))
+
+fig = plt.figure()  # 3D axis plot start !
+ax3 = plt.axes(projection='3d')
+# construct mesh
+# log scale change
+xx = np.array([np.log10(1e-3),np.log10(1e-2),np.log10(1e-1),np.log10(1e0),
+               np.log10(1e1),np.log10(2e1),np.log10(3e1),np.log10(4e1),np.log10(5e1)])
+yy = np.array([np.log10(1e-2),np.log10(1e-1),np.log10(1e0),np.log10(1e1),
+               np.log10(2e1),np.log10(3e1),np.log10(4e1),np.log10(5e1)])
+X, Y = np.meshgrid(xx, yy)
 
 
+ax3.plot_surface(X,Y,np.reshape(score_avg,[8,9]),cmap='cubehelix')
+ax3.set_ylabel('log(C)')
+ax3.set_xlabel('log(gamma)')
+ax3.set_zlabel('avg val_acc')
 
+# if u want to save img
+# plt.savefig('SVM_gamma_C.png',dpi=600)
 
+plt.show()
 
+""" The code below is for learning curve plotting (best model) in five-fold cross-validation,
+which study the relationship between the number of training samples and train_acc, val_acc, 
+just uncomment and then you can use it to plot ! """
 
+# k_list = [400,800,1200,1600,2000,2400] # define the size of the training data
+# score_avg_val = []
+# score_avg_train = []
+# for k in k_list:
+#     score_T = []
+#     score =[]
+#     for i in range(5):
+#         print("fold {}".format(i))
+#
+#         X_train = np.array(pd.read_csv('./x_train_pca_{}.csv'.format(i), header=None))
+#         y_train = np.array(pd.read_csv('./y_train_{}.csv'.format(i), header=None)).ravel()
+#         X_test = np.array(pd.read_csv('./x_test_pca_{}.csv'.format(i), header=None))
+#         y_test = np.array(pd.read_csv('./y_test_{}.csv'.format(i), header=None)).ravel()
+#         scaler = MinMaxScaler()
+#         X_train = scaler.fit_transform(X_train[:k,:])
+#         X_test = scaler.transform(X_test)
+#
+#         y_pred, y_pred_T = SVM(X_train, y_train[:k], X_test, 1e-2, 4e1)
+#
+#         score_T.append(accuracy_score(y_train[:k], y_pred_T))   # collect training data acc score
+#         score.append(accuracy_score(y_test, y_pred))  # collect test data acc score
+#         print('Accuracy: ' + str(accuracy_score(y_test, y_pred)))
+#     score_avg_val.append(np.mean(score))  # collect test data avg acc score
+#     score_avg_train.append(np.mean(score_T))  # collect training data avg acc score
+#
+# plt.plot(k_list, score_avg_val, label='val_acc')
+# plt.plot(k_list, score_avg_train, label='train_acc')
+# plt.xlabel('num of samples')
+# plt.ylabel('avg acc ')
+# plt.legend(loc=4)
+# # plt.savefig('SVM_best.png',dpi=600)
+# plt.show()
 
+#%% formal training and testing
 
+# load preprocessed training & test data
+X_train = np.array(pd.read_csv('./x_train_pca_formal.csv',header = None))
+y_train = np.array(pd.read_csv('./y_train_formal.csv',header = None)).ravel()
+X_test = np.array(pd.read_csv('./x_test_pca_formal.csv',header = None))
+y_test = np.array(pd.read_csv('./y_test_formal.csv',header = None)).ravel()
 
+# training & prediction
+y_pred, y_pred_T = SVM(X_train, y_train, X_test, 1e-2, 4e1)
+# accuracy_score(y_test, y_pred)
 
-
-
-
-
-
-
-
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
